85cb4ed87d8edd2f952e1bf2c1aad36390a193f8
johnomernik <john@omernik.com>
2018-04-23 12:48:12 -0500

    Updated Readme and instructions

--- mkstream.sh
+++ mkstream.sh
@@ -6,8 +6,8 @@ POSIX_MNT="${MAPR_MOUNT_PATH}/${MAPR_CLUSTER}"
 POSIX_VOL="${POSIX_MNT}${MAPR_SYSLOG_VOLUME_LOCATION}"
 POSIX_STREAM="${POSIX_MNT}${MAPR_SYSLOG_STREAM_LOCATION}"
 
-#MAPRCLI="maprcli"
-MAPRCLI="/home/zetaadm/homecluster/zetago/zeta fs mapr maprcli -U=mapr"
+MAPRCLI="maprcli"
+#MAPRCLI="/home/zetaadm/homecluster/zetago/zeta fs mapr maprcli -U=mapr"
 if [ ! -d "$POSIX_VOL" ]; then
     $MAPRCLI volume create -path $MAPR_SYSLOG_VOLUME_LOCATION -rootdirperms 775 -user "${MAPR_CONTAINER_USER}:fc,a,dump,restore,m,d mapr:fc,a,dump,restore,m,d" -ae $MAPR_CONTAINER_USER -name "prod.data.sssyslog"
 fi
